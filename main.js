7351fe27507c53ea0ba89e5484708d73fb7beb54
KlaraHojacova <klara.hojacova@seznam.cz>
2026-04-19 13:57:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 /**
- * Function for calculation of birthdate
+ * Function for calculation of date of birth vs age range
+ * @param {object} age Age range - min.max.
+ * @returns {string} Date in ISO format
  */
 function getBirthdate(age) {
     const msInYear = 365.25 * 24 * 60 * 60 * 1000;
     const now = new Date().getTime();
-
-    // Birthdate intervals
     const earliest = now - (age.max * msInYear);
     const latest = now - (age.min * msInYear);
-
     const randomMs = Math.floor(Math.random() * (latest - earliest + 1) + earliest);
     return new Date(randomMs).toISOString();
 }
 
 /**
  * Main function for employee generation
+ * @param {object} dtoIn Input data
+ * @returns {array} Employee list
  */
 export function main(dtoIn) {
     const result = [];
-
-    // My employee list
     const employees = [
         { name: "Jan", surname: "Holý", gender: "male" },
         { name: "Miroslav", surname: "Rezek", gender: "male" },
@@ -29,12 +28,11 @@ export function main(dtoIn) {
         { name: "Zdena", surname: "Holá", gender: "female" }
     ];
 
+    // Tady jsou ty zavitorky, ktere mi minule vypadly:
     const workloads = [10, 20, 30, 40];
 
     for (let i = 0; i < dtoIn.count; i++) {
-        // Random selection from my employee list
         const person = employees[Math.floor(Math.random() * employees.length)];
-
         result.push({
             gender: person.gender,
             birthdate: getBirthdate(dtoIn.age),
@@ -43,7 +41,5 @@ export function main(dtoIn) {
             workload: workloads[Math.floor(Math.random() * workloads.length)]
         });
     }
-
     return result;
 }
-
