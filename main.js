4b0885fb3463cceca74b04be143c49275bc9e2f1
KlaraHojacova <klara.hojacova@seznam.cz>
2026-04-19 12:21:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,42 +10,42 @@ const dtoIn = {
 const dtoOut = [
     {
         gender: "male",
-        birthdate: "1979-05-03T00:00:00.000Z",
+        birthDate: "1979-05-03T00:00:00.000Z",
         name: "Jan",
         surname: "Holý",
         workload: 40
     },
     {
         gender: "male",
-        birthdate: "1983-08-15T00:00:00.000Z",
+        birthDate: "1983-08-15T00:00:00.000Z",
         name: "Miroslav",
         surname: "Rezek",
         workload: 30
     },
     {
         gender: "male",
-        birthdate: "1981-04-03T00:00:00.000Z",
+        birthDate: "1981-04-03T00:00:00.000Z",
         name: "Jaroslav",
         surname: "Raketa",
         workload: 20
     },
     {
         gender: "male",
-        birthdate: "1975-05-06T00:00:00.000Z",
+        birthDate: "1975-05-06T00:00:00.000Z",
         name: "Rostislav",
         surname: "Holek",
         workload: 10
     },
     {
         gender: "female",
-        birthdate: "1983-01-05T00:00:00.000Z",
+        birthDate: "1983-01-05T00:00:00.000Z",
         name: "Jana",
         surname: "Bílá",
         workload: 40
     },
     {
         gender: "female",
-        birthdate: "1983-06-13T00:00:00.000Z",
+        birthDate: "1983-06-13T00:00:00.000Z",
         name: "Zdena",
         surname: "Holá",
         workload: 30
@@ -66,11 +66,11 @@ export function main(dtoIn) {
     for (let i = 0; i < dtoIn.count; i++) {
         // Calculation of age to meet the age range
         const birthYear = 2026 - Math.floor(Math.random() * (dtoIn.age.max - dtoIn.age.min + 1) + dtoIn.age.min);
-        const birthdate = new Date(Date.UTC(birthYear, 0, 1)).toISOString();
+        const birthDate = new Date(Date.UTC(birthYear, 0, 1)).toISOString();
 
         result.push({
             gender: genders[Math.floor(Math.random() * genders.length)],
-            birthdate: birthdate,
+            birthDate: birthDate,
             name: names[Math.floor(Math.random() * names.length)],
             surname: surnames[Math.floor(Math.random() * surnames.length)],
             workload: workloads[Math.floor(Math.random() * workloads.length)]
@@ -80,5 +80,3 @@ export function main(dtoIn) {
     return result;
 }
 
-// code runnig for controlling purposes
-console.log(main(dtoIn));
